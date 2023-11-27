038c7b1665d6f04efae33617692cd704f01e2d93
Younes Ounajjar <younajja@c3r10p6.1337.ma>
2023-11-27 03:59:27 +0100

    myPRINTF

---
+++ Makefile
@@ -0,0 +1,26 @@
+
+SRCS =	ft_printf.c ft_hex.c\
+		ft_putchar.c ft_putnbr.c \
+		ft_putstr.c ft_putunsigned.c \
+		ft_Upphex.c
+		
+OBJS = ${SRCS:.c=.o}
+NAME = libftprintf.a
+CC = cc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror
+
+${NAME}: ${OBJS}
+	ar rcs ${NAME} ${OBJS}
+
+all: ${NAME}
+
+clean:
+	${RM} ${OBJS}
+
+fclean: clean
+	${RM} ${NAME}
+
+re: fclean all
+
+.PHONY : all clean fclean re
---
+++ ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younajja <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 03:08:38 by younajja          #+#    #+#             */
+/*   Updated: 2023/11/27 03:46:51 by younajja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putnbr(int n);
+int	ft_putstr(char *str);
+int	ft_putunsigned(unsigned int nb);
+int	ft_hex(unsigned int n);
+int	ft_Upphex(unsigned int n);
+
+#endif
